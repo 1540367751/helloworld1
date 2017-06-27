56e75945e4b8b4f9fa4257ca897ef2707600e877
1540367751 <1540367751@qq.com>
2017-06-27 14:42:59 +0800

    Update main.js

--- main/main.js
+++ main/main.js
@@ -2,7 +2,10 @@
     console.log("Debug Info");
     return 'Hello World!';
 };*/
+var i=9,j=4;
 function main(i,j) {
     Remaindar = i % j;
     return Remaindar;
 };
+var b=main(i,j）；
+console.log(b);
