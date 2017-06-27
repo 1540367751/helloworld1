a6074ef93d4d34ddae10ad84356411ac4ea7aaa0
1540367751 <1540367751@qq.com>
2017-06-27 14:33:00 +0800

    Update main.js

--- main/main.js
+++ main/main.js
@@ -6,26 +6,3 @@ function caclRemaindar(i,j) {
     Remaindar = i % j;
     return Remaindar;
 };
-function caclSum(array) {
-    var sum = 0;
-    for (var i = 0; i < array.length; i++){
-        sum += array[i];
-    }
-    return sum;
-}
-function caclSumInConditon(array1,i) {
-    var SumInConditon = 0;
-    for (let number of array1){
-        if (number < i)
-        {
-            SumInConditon += number;
-        }
-    }
-    return SumInConditon;
-}
-
-module.exports = {
-    caclRemaindar,
-    caclSum,
-    caclSumInConditon
-}
