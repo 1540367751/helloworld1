24a2c5ab05eebb855a18ea57cf3c36aefc146e37
1540367751 <1540367751@qq.com>
2017-06-27 14:27:45 +0800

    Update main.js

--- main/main.js
+++ main/main.js
@@ -1,4 +1,7 @@
-module.exports = function main() {
+/*module.exports = function main() {
     console.log("Debug Info");
     return 'Hello World!';
-};
+};*/
+var a=9,b=4;
+var c=a%b;
+console.log(c);
